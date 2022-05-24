b29dc0a1409b7c0b25098be252f99a271ca731d0
Cooper Liu <shan3275@gmail.com>
2022-05-24 11:17:46 +0800

    log4js time without T

--- code/log4js_test.js
+++ code/log4js_test.js
@@ -1,12 +1,20 @@
 var log4js = require("log4js");
 log4js.configure({
   appenders: {
-    everything: { type: 'file', filename: 'all-the-logs.log' }
+    //everything: { type: 'file', filename: 'all-the-logs.log' }
+    everything: { 
+		type: 'stdout',
+		layout: {
+			type: 'pattern',
+			pattern: '[%d{yyyy-MM-dd hh:mm:ss.SSS}] [%p] %c - %m'
+		}
+	}
   },
   categories: {
     default: { appenders: [ 'everything' ], level: 'debug' }
   }
 });
+
 var logger = log4js.getLogger();
 logger.debug("Some debug messages");
 logger.info("info debug messages");
